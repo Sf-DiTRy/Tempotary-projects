a9f02abc655bc68b4e290f426bea1df905744b0b
DiTRy <sf.ditry@gmail.com>
2022-05-20 13:48:16 +0300

    ...

---
+++ IOS15-33.4_Module.Carthage.TabBarTest/IOS15-33.4_Module.Carthage.TabBarTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS15-33.4_Module.Carthage.TabBarTest
+//
+//  Created by DiTRy on 19.05.2022.
+//
+
+import UIKit
+import BubbleTabBar
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
